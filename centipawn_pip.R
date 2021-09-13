ea72905250dc1e952fb0bbe364cbc2d7581c3104
Patrick Coulombe <patrick.coulombe@gmail.com>
2021-09-12 22:21:09 -0400

    Add load_data() and eval functions to make mates numerical and add eval change at each ply (from timeframe repo)

--- centipawn_pip.R
+++ centipawn_pip.R
@@ -5,3 +5,10 @@ Centipawn Loss
 pip
 "
 
+source("centipawn_fn.R")
+df <- load_data(k_games = 200, use_local_file = TRUE)
+df <- replace_mates_with_extreme_evaluations(df)
+df <- add_eval_change_at_each_ply(df)
+
+
+
